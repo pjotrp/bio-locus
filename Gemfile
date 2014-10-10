f515c5ace781c62fe6219ae16d812c6e899da4f1
Pjotr Prins <pjotr.public01@thebird.nl>
2014-10-10 11:52:36 +0200

    Fix tests

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ group :development do
   gem "jeweler" 
   gem "bundler"
   gem "rspec"
+  gem "tokyocabinet"
+  gem "localmemcache"
+  gem "moneta"
 end
 # The following are optional (Ruby serialize is the default)
 # gem "tokyocabinet"
--- spec/bio-locus_spec.rb
+++ spec/bio-locus_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "BioLocus with Serialize" do
   fn = 'biolocus_serialize.db'
-  store = BioLocus::DbMapper.factory({storage: :serialize, store: fn})
+  store = BioLocus::DbMapper.factory({storage: :serialize, db: fn})
   store['test'] = 'yes'
   store['test2'] = 'no'
   a = store['test']
